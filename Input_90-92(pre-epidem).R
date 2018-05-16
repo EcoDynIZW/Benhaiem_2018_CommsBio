54bf2982adbf1d17794ba27b67406be8a78e9352
LucileMarescot <34071463+LucileMarescot@users.noreply.github.com>
2018-05-16 18:04:51 +0200

    Add files via upload

---
+++ Input_90-92(pre-epidem).R
@@ -0,0 +1,98 @@
+
+# ------------ INPUT FILE for the PRE_EPIDEMIC period 1990-1992------------
+# Equivalent to Table 1 in the main text
+
+# Demographic states were abbreviated as cubs (C), subadults (SA), breeders (B) or non-breeders (NB),
+# social states as high status (H) or low status (L) and infection states as susceptible (S), infected (I) or recovered (R).
+
+###--------- SURVIVAL PROBABILITIES
+
+# CUBS
+phiCLS <- 0.861674646  # Survival of Cub, Low-ranking & Susceptible
+phiCLI <- 0.622810147  # Survival of Cub, Low-ranking & Infected
+phiCLR <- 0            # Survival of Cub, Low-ranking & Recovered (equals to 0 because 0 recovered cub in CMR datasets)
+
+phiCHS <- 0.900786935  # Survival of Cub, High-ranking & Susceptible
+phiCHI <- 0.786418457  # Survival of Cub, High-ranking & Infected
+phiCHR <- 0            # Survival of Cub, High-ranking & Recovered
+
+# SUBADULTS
+phiSLS <- 0.899696769 # Survival of Subadult, Low-ranking & Susceptible
+phiSLI <- 0.691349361 # Survival of Subadut, Low-ranking & Infected
+phiSLR <- 0.691349361 # Survival of Subadut, Low-ranking & Recovered
+
+phiSHS <- 0.969186164  # Survival of Subadult, High-ranking & Susceptible
+phiSHI <- 0.691349361  # Survival of Subadult, High-ranking & Infected
+phiSHR <- 0.691349361  # Survival of Subadult, High-ranking & Recovered
+
+
+
+# BREEDERS
+phiBLS <- 0.946610295 # Survival of Breeder, Low-ranking & Susceptible
+phiBLI <- 0.946610295 # Survival of Breeder, Low-ranking & Infected
+phiBLR <- 0.946610295 # Survival of Breeder, Low-ranking & Recovered
+phiBHS <- 0.946610295 # Survival of Breeder, High-ranking & Susceptible
+phiBHI <- 0.946610295 # Survival of Breeder, High-ranking & Infected
+phiBHR <- 0.946610295 # Survival of Breeder, High-ranking & Recovered
+
+
+# NON-BREEDERS
+phiNBLS <- 0.863473071 # Survival of Non-Breeder, Low-ranking & Susceptible
+phiNBLI <- 0.863473071 # Survival of Non-Breeder, Low-ranking & Infected
+phiNBLR <- 0.863473071 # Survival of Non-Breeder, Low-ranking & Recovered
+phiNBHS <- 0.863473071 # Survival of Non-Breeder, High-ranking & Susceptible
+phiNBHI <- 0.863473071 # Survival of Non-Breeder, High-ranking & Infected
+phiNBHR <- 0.863473071 # Survival of Non-Breeder, High-ranking & Recovered
+
+
+### -----FECUNDITY (equal to 1 as all females in the Breeder state reproduce)
+
+fLS <- 1 # fecundity of Low-Ranking & Susceptible
+fLI <- 1 # fecundity of Low-Ranking & Infected
+fLR <- 1 # fecundity of Low-Ranking & Recovered
+fHS <- 1 # fecundity of High-Ranking & Susceptible
+fHI <- 1 # fecundity of High-Ranking & Infected
+fHR <- 1 # fecundity of High-Ranking & Recovered
+
+###---------------- SEX RATIO & LITTER SIZE
+
+ls <- 1.53 # Average litter size (see Supplementary Information 2)
+sr <- 0.52 # Sex-ratio at birth (see Supplementary Information 2)
+
+###-----------------STATE TRANSITION PROBABILITIES
+
+# SOCIAL STATES
+
+rLL.S <- 0.974790382  # Probability of staying Low-ranking for Susceptible females
+rLL.I <- 0.974790382  # Probability of staying Low-ranking for Infected females
+rLL.R <- 0.974790382  # Probability of staying Low-ranking for Recovered females
+
+rHH.S <- 0.939695149 # Probability of staying High-ranking for Susceptible females
+rHH.I <- 0.939695149 # Probability of staying High-ranking for Infected females
+rHH.R <- 0.939695149 # Probability of staying High-ranking for Recovered females
+
+# INFECTION STATES
+
+betaCL <- 0.454504272  # Probability of transition from S to I for Low-ranking Cubs
+betaCH <- 0.233491397  # Probability of transition from S to I for High-ranking Cubs
+
+betaSL <- 0.021071825  # Probability of transition from S to I for Low-ranking Subadults
+betaSH <- 0.095583371  # Probability of transition from S to I for High-ranking Subadults
+
+betaADL <- 0.021071825 # Probability of transition from S to I for Low-ranking Adults (Breeders&Non-Breeders)
+betaADH <- 0.095583371 # Probability of transition from S to I for High-ranking Adults (Breeders&Non-Breeders)
+
+
+# DEMOGRAPHIC states (i.e. accounts for recruitment)
+
+bSL <- 0.01002621 # Probability of transition that Low-ranking Subadults become Breeders
+bBL <- 0.452045588  # Probability of transition that Low-ranking Breeders become Breeders
+bNBL <-  0.602846751 # Probability of transition that Low-ranking Breeders become Breeders
+
+bSH <- 0.037954669 # Probability of transition that High-ranking Subadults become Breeders
+bBH <-  0.487935995 # Probability of transition that High-ranking Breeders become Breeders
+bNBH <-  0.680345616 # Probability of transition that High-ranking Breeders become Breeders
+
+
+
+
